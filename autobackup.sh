89a944bc943314a3c1fc3065c60e42ad7b130f84
stori_Dan <qsc810148@gmail.com>
2026-03-10 17:19:54 +0800

    Ubuntu Interface backup

--- autobackup.sh
+++ autobackup.sh
@@ -7,10 +7,11 @@ PATHS=(
     "/org/gnome/settings-daemon/plugins/media-keys/"
 )
 
-BACKUP_DIR="$HOME/Downloads/satori_backup_$(date +%Y%m%d%H%M%S)"
-# BACKUP_DIR=~/Downloads/"satori_backup_$(date +%Y%m%d%H%M%S)"
-mkdir -p "$BACKUP_DIR"
-BACKUP_FILE="$BACKUP_DIR/interface_settings.ini"
+
+# BACKUP_DIR=~/Downloads/"satori_backup_$(date +26%m%d%H%M%S)"
+BACKUP_DIR="$HOME/Downloads/satori_backup_$(date +26%m%d%H%M%S)"
+mkdir -p $BACKUP_DIR
+BACKUP_FILE="$BACKUP_DIR/interface.ini"
 echo "🚀 備份開始..."
 
 > "$BACKUP_FILE"
@@ -19,21 +20,4 @@ for p in "${PATHS[@]}"; do
     dconf dump "$p" >> "$BACKUP_FILE"
 done
 
-echo "✅ 備份完成：$BACKUP_FILE"
-
-echo "🦊 正在拷貝Firefox設定..."
-rsync -ah --info=progress2 \
-    --exclude='Crash Reports' \
-    --exclude='Pending Pings' \
-    --exclude='firefox-mpris' \
-    ~/.config/mozilla/firefox \
-    "$BACKUP_DIR/"
-echo "✅ .config備份完成。"
-
-
-
-cp -r ~/.ssh "$BACKUP_DIR"
-echo "✅ SSH金鑰備份完成。"
-
-echo "✨ 備份成功！你的設定已安全存放在：$BACKUP_DIR"
-echo "Namaste. 守護智慧邊界，從備份開始。"
+echo "✨ 備份成功！你的設定已安全存放在：$BACKUP_FILE"
